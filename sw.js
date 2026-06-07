1ff2244c5466ca264c2a3b6f66c59cd15008cdf4
Tnas76hub <a31779@aecister.pt>
2026-06-07 17:13:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,6 @@ self.addEventListener('fetch', (e) => {
 
 // Função para abrir a janela com a imagem
 function abrirModal() {
-    alert("O botão foi clicado com sucesso!");
     document.getElementById("meuModal").style.display = "block";
 }
 
